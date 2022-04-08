f4bb2b665af42d4eedf27c23c648ebeebeb7f4e1
Jair Brann Junior <jairjunior02@hotmail.com>
2022-04-08 20:39:22 -0300

    inicio

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,4 +5,4 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`)) //jajajaja
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
