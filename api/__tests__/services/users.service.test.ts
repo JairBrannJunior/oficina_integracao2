d522fd714108a2039b9b7fa28fa0e26cf2abac21
João Wicktor <joaowicktor@gmail.com>
2022-04-28 23:30:58 -0300

    test users service

--- api/__tests__/services/users.service.test.ts
+++ api/__tests__/services/users.service.test.ts
@@ -1,20 +1,12 @@
 import bcrypt from 'bcrypt';
-import User from '../../src/models/User';
+import usersRepository from '../../src/repositories/users.repository';
 import usersService from '../../src/services/users.service';
-import { Exception } from '../../src/utils/exception';
-
-jest.mock('../../src/services/users.service');
-// jest.mock('../../src/models/User', () => ({
-//   findOne: jest.fn(),
-// }));
 
 describe('UsersService', () => {
-  const userModelMocked = User as jest.Mocked<typeof User>;
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  const mockedUsersService = usersService as jest.Mocked<typeof usersService>;
   it('should create a user', async () => {
     const user = {
       name: 'test',
@@ -29,41 +21,34 @@ describe('UsersService', () => {
       password: await bcrypt.hash(user.password, 10),
     };
 
-    mockedUsersService.createUser.mockResolvedValueOnce(insertedUser);
+    jest.spyOn(usersService, 'createUser').mockReturnValueOnce(Promise.resolve(insertedUser));
 
-    expect(await mockedUsersService.createUser(user)).toBe(insertedUser);
+    expect(await usersService.createUser(user)).toBe(insertedUser);
   });
 
-  it('should throw an error when creating a user', async () => {
+  it('should throw an error when user already exists', async () => {
     const user = {
       name: 'test',
       email: 'test@test.com',
       password: 'test',
     };
 
-    jest.spyOn(User, 'findOne').mockReturnValue(
+    jest.spyOn(usersRepository, 'findByEmail').mockReturnValueOnce(
       Promise.resolve({
         id: 1,
         name: 'test',
         email: 'test@test.com',
-      } as any)
+      })
     );
 
-    // userModelMocked.findOne.mockReturnValue({
-    //   id: 1,
-    //   name: 'test',
-    //   email: 'test@test.com',
-    // } as any);
+    let existingUser;
+    try {
+      existingUser = await usersService.createUser(user);
+    } catch (error: any) {
+      expect(error.statusCode).toBe(409);
+      expect(error.message).toBe('User already exists');
+    }
 
-    // mockedUsersService.createUser.mockImplementation(() => {
-    //   throw new Exception({
-    //     status: 409,
-    //     message: 'User already exists',
-    //   });
-    // });
-
-    console.log(await mockedUsersService.createUser(user));
-
-    expect(await usersService.createUser(user)).toBe(undefined);
+    expect(existingUser).toBeUndefined();
   });
 });
