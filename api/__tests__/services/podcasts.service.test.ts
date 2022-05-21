516f4908b25b832fb35e2c9bd21e14f229af218b
João Wicktor <joaowicktor@gmail.com>
2022-05-21 16:04:25 -0300

    added get podcast by id tests

--- api/__tests__/services/podcasts.service.test.ts
+++ api/__tests__/services/podcasts.service.test.ts
@@ -41,5 +41,36 @@ describe('PodcastsService', () => {
     jest.spyOn(podcastsRepository, 'listByUserId').mockReturnValueOnce(Promise.resolve([]) as any);
 
     expect(await podcastsService.listPodcastsByUser(userId)).toEqual([]);
-  })
+  });
+
+  it('should get podcast by id', async () => {
+    const podcastId = 1;
+
+    const podcast = {
+      id: 1,
+      title: 'test',
+      description: 'test',
+      publishedAt: new Date(),
+      fileUrl: 'http://test.com/test.mp3',
+      duration: 4237,
+      userId: 1,
+    };
+
+    jest.spyOn(podcastsRepository, 'getById').mockReturnValueOnce(Promise.resolve(podcast) as any);
+
+    expect(await podcastsService.getPodcastById(podcastId)).toEqual(podcast);
+  });
+
+  it('should throw exception when podcast not found', async () => {
+    const podcastId = 1;
+
+    jest.spyOn(podcastsRepository, 'getById').mockReturnValueOnce(Promise.resolve(null) as any);
+
+    try {
+      await podcastsService.getPodcastById(podcastId);
+    } catch (error: any) {
+      expect(error.statusCode).toEqual(404);
+      expect(error.message).toEqual('Podcast not found');
+    }
+  });
 });
