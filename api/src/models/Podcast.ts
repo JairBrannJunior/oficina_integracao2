c60a0897b420596a70e74bc0d811ff5272c9333d
João Wicktor <joaowicktor@gmail.com>
2022-05-21 14:12:32 -0300

    added podcast model and associate to user

---
+++ api/src/models/Podcast.ts
@@ -0,0 +1,68 @@
+import { BuildOptions, DataTypes, Model } from 'sequelize';
+import { sequelize } from '../database';
+import User from './User';
+
+interface PodcastAttributes {
+  id: number;
+  title: string;
+  members: string;
+  publishedAt: Date;
+  thumbnail: string;
+  description: string;
+  fileUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface PodcastInstance extends Model<PodcastAttributes>, PodcastAttributes {}
+
+type PodcastModelStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): PodcastInstance;
+};
+
+const Podcast = sequelize.define(
+  'podcast',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    members: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    publishedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    thumbnail: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    fileUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'podcasts',
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+  }
+) as PodcastModelStatic;
+
+export default Podcast;
